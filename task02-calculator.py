11f42e68ff97ae57ab24d65ff39f9553733e436a
Hozaifa-the-programmer <148899241+Hozaifa-the-programmer@users.noreply.github.com>
2023-10-24 23:26:58 +0530

    Create task02-calculator.py

---
+++ task02-calculator.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+opr=input("Enter the opr..")
+if opr=="+":
+    print(num1+num2)
+elif opr=="-":
+    print(num1-num2)
+elif opr=="*":
+    print(num1*num2)
+elif opr=="/":
+    print(num1/num2)            
+else:
+    print("invalid opr....")                    
